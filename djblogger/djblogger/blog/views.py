d366cae1675959c9c3802523a6fd1c062220eb13
Gevorg7 <64933888+Gevorg7@users.noreply.github.com>
2023-08-27 00:12:28 +0400

    Added homepage

--- djblogger/djblogger/blog/views.py
+++ djblogger/djblogger/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from djblogger.blog.models import Post
+
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'blog/index.html'
